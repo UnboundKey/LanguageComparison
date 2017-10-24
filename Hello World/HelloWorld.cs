9cc8cc871c4d56cbaa57459fb9cd60ced7a42204
UnboundKey <godiset@gmail.com>
2017-10-24 10:54:46 +0200

    Added Hello World - C#

---
+++ Hello World/HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorldCSharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
